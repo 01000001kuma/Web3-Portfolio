4fbe976bc538d43b128dd0dc3fe6a2a60f5bbbb4
01000001kuma <a_illera@yahoo.com>
2023-03-15 19:04:26 +0100

    Setting up enviroment and dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <div>
-      3D Developer Portfolio Website
+      3D Developer Portfolio
     </div>
   )
 }
