ca6627bccf233c876e3f6b89a31d3a2acc4de066
01000001kuma <a_illera@yahoo.com>
2023-03-16 18:05:01 +0100

    computer model and dependencies

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,9 +6,43 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
 const Computers = () => {
+  const comuputer = useGLTF('./desktop_pc/scene.gltf')
+
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15}
+      groundColor='black' />
+      <primitiveLight intensity={1} />
+      <primitive object={comuputer.scene} />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return(
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}     
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+        enableZoom={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+
+      <Preload all />
+
+    </Canvas>
+
+  )
+
+
+}
+
+export default Computers 
