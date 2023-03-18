f22fe4ef71510fc737af155e72e159c118c78b12
01000001kuma <a_illera@yahoo.com>
2023-03-18 13:40:47 +0100

    scale logic of the 3d model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,7 +5,7 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
 
 import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ( isMobile ) => {
   const comuputer = useGLTF('./desktop_pc/scene.gltf')
 
 
@@ -24,8 +24,8 @@ const Computers = () => {
       />
       <primitive 
         object={comuputer.scene} 
-        scale={0.75}
-        position={[0, -3.25, -1.5 ]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -0.2] : [0, -3.25 -1.5 ]}
         rotation={[-0.01, -0.2, -0.1]}
       
       />
@@ -34,6 +34,32 @@ const Computers = () => {
 }
 
 const ComputersCanvas = () => {
+  const [ isMobile, setIsMobile ] = useState(false);
+
+  useEffect(() => {
+    // add event listener that listen to the screen size
+    const mediaQuery = windoy.matchMedia('(max-width: 500px)');
+
+    // set the initial value of the isMobile state variable
+
+    setIsMobile(mediaQuery.matches);
+
+    // define a callback function that will be called when the screen size changes
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // add the callback as a listerner for changes to the media query
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange); 
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+
+  } , [])
+
   return(
     <Canvas
       frameloop='demand'
@@ -47,7 +73,7 @@ const ComputersCanvas = () => {
         maxPolarAngle={Math.PI / 2}
         minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
 
       <Preload all />
