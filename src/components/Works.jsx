8e0d06c510cbfaf435882483d1383caa6690a06f
01000001kuma <a_illera@yahoo.com>
2023-03-19 09:50:46 +0100

    wip works section component

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import Tilt from 'react-tilt'
+import { Motion } from 'react-motion'
+
+import { Styles } from '../styles'
+import { github } from '../assets'
+import { SectionWrapper } from '../hoc'
+import { projects } from '../constants'
+import { fadeIn, textVarian } from '../utils/motion'
+
 
 const Works = () => {
   return (
-    <div>Works</div>
+    <>
+      <motion.div variants={textVariants()}>
+         <p className={styles.sectionSubText}>What I have done so far</p>
+        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+      </motion.div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, '')
