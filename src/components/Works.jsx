7f39ba280e6ac26065ed04419fb4c9b73bb567d9
01000001kuma <a_illera@yahoo.com>
2023-03-19 10:50:00 +0100

    work section done?

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -9,8 +9,51 @@ import { fadeIn, textVarian } from '../utils/motion'
 
 const ProjectCard = ({ index, name, description, tags, image, source_code_link }) => {
   return ( 
-    <motion.div>
-      test
+    <motion.div variants={fadeIn('up', 'spring', index * 0.5, 0.75)} > 
+      <Tilt
+        options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+        }}
+        className='bg-terntiary p-5 rounded-2xl sm:w-[360px] w-full'
+      >
+        <div className='relative w-full h-[230px]'>
+          <img 
+            src={image}
+            alt={name}
+            className='w-full h-full object-cover rounded-2xl'
+          />
+
+          <div className='absolute insert-0 flex justify-end m-3 card-img_hover'>
+            <div  
+              onClick={() => window.open(source_code_link, '_blank')}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            >
+              <img src={github} alt="github" className='w-1/2 h-1/2 object-contain' />
+
+              {/* If Want to insert a live icon to redirect to the live progert website (need to change the icon) video time 1.55.20 */}
+              {/* <div  
+              onClick={() => window.open(source_code_link, '_blank')}
+              className='black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer'
+            >
+              <img src={github} alt="github" className='w-1/2 h-1/2 object-contain' />  */}
+
+            </div>
+          </div>
+        </div>
+        <div className='mt-5'>
+          <h3 className='text-white text-[24px] font-bold'>{name}</h3>
+          <p className='text-secondary text-[14px]  mt-2'>{description}</p>
+        </div>
+        <div className='mt-4 flex flex-wrap gap-2'>
+          {tags.map((tag) => ( 
+            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+              #{tag.name}
+            </p>
+          ))}
+        </div>
+      </Tilt>
     </motion.div>
   )
 }
