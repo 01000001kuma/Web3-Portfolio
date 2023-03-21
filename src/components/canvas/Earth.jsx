cd27e8f5bb91a413e6691386f85a3a8362e8ef9b
01000001kuma <a_illera@yahoo.com>
2023-03-21 16:13:47 +0100

    3d earth model

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+
+import CanvasLoader from '../Loader'
 
 const Earth = () => {
   return (
     <div>Earth</div>
+  )
+}
+
+const EarthModel = () => {
+
+  return (
+    <Canvas
+      shadows
+      frameloop='demand'
+      gl={{oreserveDrawingBuffer: true}}
+      camera={{}}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        
+      </Suspense>  
+    </Canvas>
   )
 }
 
